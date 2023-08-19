dcabf5bb4ad2b4412227338b085dfc2e8f513613
InvalidSE <taine.reader@gmail.com>
2023-08-19 20:47:15 +1200

    changes like that one song by what artist that was

--- dashboard/src/lib/components/NewsSourceCard.svelte
+++ dashboard/src/lib/components/NewsSourceCard.svelte
@@ -3,17 +3,23 @@
     export let source: NewsSource;
     import { onMount } from 'svelte';
 
+    // on click send to /site/id 
+    function handleClick() {
+        window.location.href = `/site/${source.id}`;
+    }
 </script>
 
 <div>
     <h1>{source.name}</h1>
-    <h1>{source.value}</h1>
+    <h2>{source.value}</h2>
+
+    <button on:click={handleClick}>View</button>
 </div>
 
 
 <style lang="scss">
     div {
-        background-color: #fff;
+        background-color: #fff; 
         border-radius: 10px;
         padding: 10px;
         margin: 10px;
@@ -22,6 +28,20 @@
         display: flex;
         flex-direction: column;
         align-items: center;
+        text-align: center;
         justify-content: center;
+        box-shadow: 0px 0px 10px 0px rgba(0,0,0,0.3);
+    }
+    button {
+        background-color: #16697a;
+        color: #fff;
+        border: none;
+        border-radius: 5px;
+        padding: 10px;
+        margin: 10px;
+        width: 100px;
+        height: 50px;
+        font-size: 1.2em;
+        cursor: pointer;
     }
 </style>
