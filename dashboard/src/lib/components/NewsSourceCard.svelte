97f59c966954516d6f7d6cc4f50c23240a96d4d9
InvalidSE <taine.reader@gmail.com>
2023-08-19 17:53:02 +1200

    logos and such

--- dashboard/src/lib/components/NewsSourceCard.svelte
+++ dashboard/src/lib/components/NewsSourceCard.svelte
@@ -3,33 +3,11 @@
     export let source: NewsSource;
     import { onMount } from 'svelte';
 
-    import chartjs from 'chart.js/auto';
-	let chartValues = [20, 10, 5, 2, 20, 30, 45];
-	let chartLabels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
-    let ctx;
-	let chartCanvas;
-    
-    onMount(async () => {
-		  ctx = chartCanvas.getContext('2d');
-			var chart = new chartjs(ctx, {
-				type: 'line',
-				data: {
-						labels: chartLabels,
-						datasets: [{
-								label: 'Revenue',
-								backgroundColor: 'rgb(255, 99, 132)',
-								borderColor: 'rgb(255, 99, 132)',
-								data: chartValues
-						}]
-				}
-		});
-	});
 </script>
 
 <div>
     <h1>{source.name}</h1>
     <h1>{source.value}</h1>
-    <canvas bind:this={chartCanvas} id="myChart"></canvas>
 </div>
 
 
